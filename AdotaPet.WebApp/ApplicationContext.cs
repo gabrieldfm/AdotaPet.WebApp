7cf562afc5b61ef6d3a1c209a85de6b2724076b9
Unknown <gabrieldfm13@gmail.com>
2019-04-27 22:55:32 -0300

    Criação
    Criação do projeto AdotaPet

---
+++ AdotaPet.WebApp/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdotaPet.WebApp
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-5P677KP4;Initial Catalog=Db_AdotaPet;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ AdotaPet.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdotaPet.WebApp.MvcApplication" Language="C#" %>
