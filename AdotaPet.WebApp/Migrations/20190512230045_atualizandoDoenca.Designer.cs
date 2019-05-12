335e36db5a061bc07b9f379fa7e4fbea100a595c
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2019-05-12 20:18:13 -0300

    Criado o os campos Ong_Id  e Grupo

--- AdotaPet.WebApp/Migrations/20190512230045_atualizandoDoenca.Designer.cs
+++ AdotaPet.WebApp/Migrations/20190512230045_atualizandoDoenca.Designer.cs
@@ -4,14 +4,16 @@ using AdotaPet.WebApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdotaPet.WebApp.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190512230045_atualizandoDoenca")]
+    partial class atualizandoDoenca
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AdotaPet.WebApp/Migrations/20190512230045_atualizandoDoenca.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdotaPet.WebApp.Migrations
+{
+    public partial class atualizandoDoenca : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Grupo",
+                table: "Doenca",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Grupo",
+                table: "Doenca");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Observacao",
+                table: "Doenca",
+                maxLength: 250,
+                nullable: true);
+        }
+    }
+}
